16a41b87a2201d81ec82effcf5857b15169bdd22
kyrp <kyawmyohtut@innovix-solutions.com>
2022-06-12 13:25:55 +0630

    support default col

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ import React from "react";
 import { Dimensions, View } from 'react-native';
 
 const Grid = props => {
+  const col = props.col || 1
   const win_width = Dimensions.get('window').width;
   const space = ((props.space || 0) / win_width) * 100;
-  const width = `${((100 / props.col) - space) + (space / props.col)}%`;
-  const marginRight = id => (id + 1) % props.col === 0 ? 0 : `${space}%`;
+  const width = `${((100 / col) - space) + (space / col)}%`;
+  const marginRight = id => (id + 1) % col === 0 ? 0 : `${space}%`;
   return <View style={{
     width: "100%",
     flexWrap: 'wrap',
