d109557b85ec64eee36132fde5ed5b53604e135d
kyrp <kyawmyohtut@innovix-solutions.com>
2022-07-05 13:57:06 +0630

    width preceding

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const lastCol = (col, id) => id + 1 >= col && (id + 1) % col === 0
 const Grid = props => {
   const deviceWidth = Dimensions.get('window').width
   const col = props.col || props.template?.length || 1
-  const space = (((props.space || 0) / deviceWidth) * 100)
-  const width = w => w - ((space * (col - 1)) / col)
+  const space = Math.floor(((props.space || 0) / deviceWidth) * 100)
+  const width = w => Math.floor(w - ((space * (col - 1)) / col))
   const partialOfTempalte = props.template ? props.template.reduce((partialSum, a) => partialSum + a, 0) : 0
   const Wrapper = Container[props.scroll ? "Scrollable" : "Default"]
 
